acca7717c98d6a6867a3fd2e460e221beb3c9ed6
Ori Roth <ori.rothh@gmail.com>
2022-11-21 00:14:57 +0200

    pytypes uses isinstance only with forward references (??)

--- motivation/dynamic/inf_sub_pytypes.py
+++ motivation/dynamic/inf_sub_pytypes.py
@@ -3,7 +3,7 @@
 # Pytypes version: 1.0b10
 # Site: https://pypi.org/project/pytypes/
 # Verdict: run time error
-# Reason: refers to isinstance which does not support generic types
+# Reason: refers to isinstance which does not support generic types (only when using resolve_fw_decl?)
 
 from typing import TypeVar, Generic
 from pytypes import is_subtype, resolve_fw_decl
